f93d8c6b45460a4738a3c7d588ba2c738f39ec16
Kelly <2035268@cognizant.com>
2021-10-28 18:45:10 -0400

    first commit

---
+++ src/test/java/com/Kelly/RecordsByDaylight/KellyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kelly.RecordsByDaylight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KellyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
